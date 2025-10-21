fb628354ac72ac2e9291f854543775d9016ba76a
Harsh Arora <harshpunyani20@gmail.com>
2025-10-21 12:55:56 +0530

    data

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Client } from "pg"
+import express from 'express'
+const app = express();
+app.use(express.json());
+
+const pgClient = new Client(process.env.POSTGRES_URL);
+
+pgClient.connect();
+
+app.post("/signup", async function(req, res) {
+    const username = req.body.username;
+    const passsword = req.body.passsword;
+    const email = req.body.email;
+    
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+
+    try {
+    const insertQuery = `INSERT INTO users (username, email, passsword) VALUES ($1, $2, $3) RETURNING id;`
+    const response = await pgClient.query(insertQuery, [username, email, passsword]);
+    const userId = response.rows[0].id;
+    const addressInsertQuery = `INSERT INTO address (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`
+    const addressInsertResponse = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
+
+    res.json({
+        msg: "you have signed up"
+    })
+    console.log(response);
+} catch(e) {
+    console.log(e);
+    res.status(400).json({
+        msg: "error while signed up"
+    })
+}
+})
+        
+
+app.listen(3000)
